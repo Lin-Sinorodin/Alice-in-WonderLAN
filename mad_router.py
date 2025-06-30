d18d30519999a5b217dc18825379d8b79d071ea7
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2025-06-30 12:08:29 +0300

    Convert code to a Router class

--- mad_router.py
+++ mad_router.py
@@ -3,20 +3,24 @@ from utils import get_routing_interfaces
 
 DUMMY_MAC = "aa:aa:aa:aa:aa:aa"
 
+class Router:
+	def __init__(self, src_iface: scapy.NetworkInterface, dst_iface: scapy.NetworkInterface):
+		self.src_iface = src_iface
+		self.dst_iface = dst_iface
 
-def redirect_packet(packet: scapy.Ether, iface: scapy.NetworkInterface) -> None:
-	"""Redirect the given packet to the given interface, only if it has layer3 payload."""
-	if packet.haslayer(scapy.IP):
-		new_ether = scapy.Ether(src=iface.mac, dst=DUMMY_MAC)
-		scapy.sendp(new_ether / packet.payload, iface=iface.name)
-
-
-def redirect_all(src_iface: str, dst_iface: str) -> None:
-	"""Redirect all packets on src_iface to dst_iface."""
-	scapy.sniff(iface=src_iface.name, prn=lambda p: redirect_packet(p, dst_iface))
+	def redirect_packet(self, packet: scapy.Ether) -> None:
+		"""Redirect the given packet to the dst interface, only if it has layer3 payload."""
+		if packet.haslayer(scapy.IP):
+			new_ether = scapy.Ether(src=self.dst_iface.mac, dst=DUMMY_MAC)
+			scapy.sendp(new_ether / packet.payload, iface=self.dst_iface.name)
+	
+	def redirect_all(self) -> None:
+		"""Redirect all packets on src_iface to dst_iface."""
+		scapy.sniff(iface=self.src_iface.name, prn=lambda p: self.redirect_packet(p))
 
 
 if __name__ == "__main__":
 	src_iface, dst_iface = get_routing_interfaces()
-	redirect_all(src_iface, dst_iface)
+	router = Router(src_iface, dst_iface)
+	router.redirect_all()
 
